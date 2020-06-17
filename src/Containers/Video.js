ee25f9c259f9f40483cb5ecbcd4fd68d8e852dc8
hanmihyeon <ilikeyou2169@gmail.com>
2020-06-17 10:59:07 +0900

    0617 AM 11:00 chart, video section 수정

--- src/Containers/Video.js
+++ src/Containers/Video.js
@@ -1,18 +1,77 @@
 import React, {Fragment} from 'react';
-import {Breadcrumb} from 'antd';
-import {LaptopOutlined,VideoCameraOutlined} from '@ant-design/icons';
+import {Breadcrumb, Row, Col} from 'antd';
+import {LaptopOutlined, VideoCameraOutlined} from '@ant-design/icons';
+import {Player} from 'video-react';
+import 'video-react/dist/video-react.css';
 
-const Video = () => {
+const Video = (props) => {
     return (
         <Fragment>
             <div style={{
-                    marginLeft: '15px'
+                    margin: '0 15px'
                 }}>
                 <Breadcrumb>
                     <Breadcrumb.Item><LaptopOutlined/>&nbsp;&nbsp;Multimedia</Breadcrumb.Item>
                     <Breadcrumb.Item><VideoCameraOutlined/>&nbsp;Video</Breadcrumb.Item>
                 </Breadcrumb>
-                <div style={{marginTop:'5px'}}>Video</div>
+                <div style={{
+                        marginTop: '20px'
+                    }}>
+                    <Row gutter={12}>
+                        <Col span={16}>
+                            <Player
+                                playsInline="playsInline"
+                                src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+                                autoPlay={true}
+                                muted={true}
+                                />
+                        </Col>
+                        <Col span={8}>
+                            <Row>
+                                <Col span={12}>
+                                    <Player
+                                        playsInline="playsInline"
+                                        src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+                                        autoPlay={true}
+                                        muted={true}/>
+                                </Col>
+                                <Col span={12}>
+                                    <Player
+                                        playsInline="playsInline"
+                                        src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+                                        autoPlay={true}
+                                        muted={true}/>
+                                </Col>
+                            </Row>
+                            <Row gutter={[0,8]}>
+                                <Col span={12}>
+                                    <Player
+                                        playsInline="playsInline"
+                                        src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+                                        autoPlay={true}
+                                        muted={true}/>
+                                </Col>
+                                <Col span={12}>
+                                    <Player
+                                        playsInline="playsInline"
+                                        src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+                                        autoPlay={true}
+                                        muted={true}/>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col span={24}>
+                                    <Player
+                                        playsInline="playsInline"
+                                        src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+                                        autoPlay={true}
+                                        muted={true}/>
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+
+                </div>
             </div>
         </Fragment>
     );
